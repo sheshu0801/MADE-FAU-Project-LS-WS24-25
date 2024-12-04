4fe4b1cc6400b4a68c36a877fc64b3127c5c0134
sheshu0801 <113191421+sheshu0801@users.noreply.github.com>
2024-12-04 16:00:11 +0100

    Add files via upload

---
+++ project/tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Running the data pipeline
+python3 pipeline_script.py
+
+# Check if the output CSV file exists
+if [ -f "/Users/sheshukumar/data/raw_csv/Yearly_Aggregated_Unemployment_Crime_Data.csv" ]; then
+    echo "CSV output file exists. Test passed."
+else
+    echo "CSV output file does not exist. Test failed."
+    exit 1
+fi
+
+# Check if the SQLite database exists
+if [ -f "./data/Unemployment_Crime_Data.sqlite" ]; then
+    echo "SQLite database exists. Test passed."
+else
+    echo "SQLite database does not exist. Test failed."
+    exit 1
+fi
+
+echo "All tests passed."
